693f502a1eb78e8518f3a44e98f76a8d1c578231
Richa Dhanuka <richa.dhanuka@gmail.com>
2024-09-11 23:16:21 +0530

    Add files via upload

---
+++ math_main.py
@@ -0,0 +1,12 @@
+# main.py
+
+import math_utils
+
+
+def main():
+    numbers = [1,2,3,4]
+    total = math_utils.calculate_sum(numbers)
+    math_utils. display_sum(total)
+
+if __name__ == "__main__":
+    main()
